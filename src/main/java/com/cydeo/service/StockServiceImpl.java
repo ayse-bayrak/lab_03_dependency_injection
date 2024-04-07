9b526f8fac9bbb05acb5cb4b9c945e6d33c729b7
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-04-07 17:34:30 +0300

    little changes

--- src/main/java/com/cydeo/service/StockServiceImpl.java
+++ src/main/java/com/cydeo/service/StockServiceImpl.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class StockServiceImpl implements StockService {
-
 
     @Override
     public boolean checkStockIsAvailable(Product product, int quantity) {
